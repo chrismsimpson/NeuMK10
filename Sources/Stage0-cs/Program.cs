625e5c8c124815e4f5edeaaac64499046941bd2b
Chris Simpson <chris.m.simpson@icloud.com>
2022-01-04 09:34:34 +1100

    -

---
+++ Sources/Stage0-cs/Program.cs
@@ -0,0 +1,11 @@
+
+public static partial class Program {
+
+    public static void Main(
+        String[] args) {
+
+        
+
+        WriteLine($"Hello foo!");
+    }
+}
---
+++ Sources/Stage0-cs/Usings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Text;
+
+global using static System.Char;
+global using static System.Console;
+global using static System.DateTimeOffset;
+global using static System.String;
